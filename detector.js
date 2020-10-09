032e282bba26057bb22dddb4b2dfd3579ae41474
Vladimir Mandic <mandic00@live.com>
2020-10-09 10:56:50 -0400

    update

--- detector.js
+++ detector.js
@@ -1,30 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 const log = require('@vladmandic/pilogger');
+// const tf = require('@tensorflow/tfjs-node-gpu');
 const tf = require('@tensorflow/tfjs-node');
 const canvas = require('canvas');
 
+// app options
 const debug = false;
+const imgInput = 'inputs/';
+const imgOutput = 'outputs/';
+const outSize = 1920; // set to 0 to avoid image resizing
+
+// model options
 const minScore = 0.2;
 const maxResults = 50;
 const iouThreshold = 0.1;
-const outSize = 1920; // set to 0 to avoid image resizing
-const imgInput = 'inputs/';
-const imgOutput = 'outputs/'
 
 const coco = JSON.parse(fs.readFileSync('./coco.json'));
 // const openimages = JSON.parse(fs.readFileSync('./openimages.json'));
-
-const defaultFont = 'small-caps 12px "Segoe UI"';
 const models = [];
+let performances = {};
 
-function time(t1, t0) {
-  return Math.trunc(parseInt(t1 - t0) / 1000 / 1000);
+function perf(model, time) {
+  const t = Math.trunc(parseInt(time) / 1000 / 1000);
+  if (!performances[model]) performances[model] = { total: 0, count: 0, avg: 0, max: 0 };
+  performances[model].total += t;
+  performances[model].count += 1;
+  performances[model].avg = Math.trunc(1.0 * performances[model].total / performances[model].count);
+  performances[model].max = performances[model].max > t ? performances[model].max : t;
 }
 
-function rect({ canvas = null, x = 0, y = 0, width = 0, height = 0, radius = 8, lineWidth = 2, color = 'white', title = null, font = null }) {
-  if (!canvas) return;
-  const ctx = canvas.getContext('2d');
+function rect({ drawCanvas = null, x = 0, y = 0, width = 0, height = 0, radius = 8, lineWidth = 2, color = 'white', title = null, font = 'small-caps 14px "Segoe UI"' }) {
+  const ctx = drawCanvas.getContext('2d');
   ctx.lineWidth = lineWidth;
   ctx.beginPath();
   ctx.moveTo(x + radius, y);
@@ -41,22 +48,21 @@ function rect({ canvas = null, x = 0, y = 0, width = 0, height = 0, radius = 8, 
   ctx.stroke();
   ctx.lineWidth = 1;
   ctx.fillStyle = color;
-  ctx.font = font || defaultFont;
+  ctx.font = font;
   if (title) ctx.fillText(title, x + 4, y + 12);
 }
 
-function getTensorFromImage(path, dtype) {
-  if (!fs.existsSync(path)) {
-    log.error('Not found:', path);
+function getTensorFromImage(image, dtype) {
+  if (!fs.existsSync(image)) {
+    log.error('Not found:', image);
     return null;
   }
-  // load binary data
-  const data = fs.readFileSync(path);
-  // decode jpeg
-  const tfimage = tf.node.decodeImage(data);
+  // load & decode jpeg
+  const data = fs.readFileSync(image);
+  const bufferT = tf.node.decodeImage(data);
   // add dimension as detection requires 4d tensor
-  const expanded = tf.expandDims(tfimage, 0);
-
+  const expanded = tf.expandDims(bufferT, 0);
+  // cast if needed
   let imageT;
   if ((dtype === 'float16') || (dtype === 'float32') || (dtype === 'DT_FLOAT')) {
     const casted = tf.cast(expanded, 'float32');
@@ -65,17 +71,16 @@ function getTensorFromImage(path, dtype) {
   } else {
     imageT = tf.clone(expanded);
   }
-  tf.dispose(expanded);
-
+  // return image tensor
   imageT.file = path;
-  imageT.bytes = tfimage.size;
-  log.info('Image:', imageT.file, imageT.bytes, 'bytes with shape:', imageT.shape, 'dtype:', dtype);
-  tf.dispose(tfimage);
+  if (debug) log.info('Image:', imageT.file, bufferT.size, 'bytes with shape:', imageT.shape, 'dtype:', dtype);
+  tf.dispose(expanded);
+  tf.dispose(bufferT);
   return imageT;
 }
 
 async function saveProcessedImage(inImage, outImage, data) {
-  if (!data || !data[0]) return;
+  if (!data || !data[0]) return false;
   return new Promise(async (resolve) => {
     // create canvas
     const scale = outSize > 0 ? (data[0].image.width / outSize) : 1;
@@ -85,10 +90,10 @@ async function saveProcessedImage(inImage, outImage, data) {
     const image = await canvas.loadImage(inImage);
     ctx.drawImage(image, 0, 0, c.width, c.height);
     // draw image title
-    rect({ canvas: c, x: 0, y: 0, width: 0, height: 0, title: data[0].model });
+    rect({ drawCanvas: c, x: 0, y: 0, width: 0, height: 0, title: data[0].model });
     // draw all detected objects
     for (const obj of data) {
-      rect({ canvas: c, x: obj.rect.x / scale, y: obj.rect.y / scale, width: obj.rect.width / scale, height: obj.rect.height / scale, title: `${Math.round(100 * obj.score)}% ${obj.class}` });
+      rect({ drawCanvas: c, x: obj.rect.x / scale, y: obj.rect.y / scale, width: obj.rect.width / scale, height: obj.rect.height / scale, title: `${Math.round(100 * obj.score)}% ${obj.class}` });
     }
     // write canvas to jpeg
     const out = fs.createWriteStream(outImage);
@@ -111,7 +116,7 @@ async function processPrediction(res, image, model, labels) {
   const scores = await res[model.map['detection_scores']].data();
   const boxes = await res[model.map['detection_boxes']].array();
   // sort & filter results
-  const overlapT = await tf.image.nonMaxSuppressionAsync(boxes[0], scores, maxResults, iouThreshold, minScore)
+  const overlapT = await tf.image.nonMaxSuppressionAsync(boxes[0], scores, maxResults, iouThreshold, minScore);
   const overlap = await overlapT.data();
   tf.dispose(overlapT);
   const results = [];
@@ -119,7 +124,7 @@ async function processPrediction(res, image, model, labels) {
   for (const i in overlap) {
     const id = parseInt(i);
     results.push({
-      image: { file: image.file, bytes: image.bytes, width: image.shape[2], height: image.shape[1] },
+      image: { file: image.file, width: image.shape[2], height: image.shape[1] },
       score: Math.trunc(10000 * scores[i]) / 10000,
       classId: classes[id],
       class: labels[classes[id]]?.displayName || labels[id]?.displayName || `UNKNOWN:${id}`,
@@ -130,186 +135,140 @@ async function processPrediction(res, image, model, labels) {
         y: Math.trunc(boxes[0][id][0] * image.shape[1]),
         width: Math.trunc((boxes[0][id][3] - boxes[0][id][1]) * image.shape[2]),
         height: Math.trunc((boxes[0][id][2] - boxes[0][id][0]) * image.shape[1]),
-      }
-    })
+      },
+    });
   }
   return results;
 }
 
 async function processSavedModel(image, modelPath) {
-  log.info('Analyzing saved model:', modelPath);
-  const t0 = process.hrtime.bigint();
-
-  let model;
   if (!models[modelPath]) {
-    // build model map
-    const def = (await tf.node.getMetaGraphsFromSavedModel(modelPath))[0];
-    if (debug) log.data('Model signature:', def)
+    log.state('Loading saved model:', modelPath);
+    // get model signature
+    const meta = await tf.node.getMetaGraphsFromSavedModel(modelPath);
+    const def = meta[0];
+    if (debug) log.data('Model signature:', def);
     const tags = def['tags'];
-    const signature = Object.keys(def.signatureDefs)[0]
-    let outputs = def.signatureDefs[signature].outputs;
-    const map = {};
-    for (let i in Object.keys(outputs)) map[Object.keys(outputs)[i]] = i;
-    if (debug) log.data('Model output map:', map);
-
+    const signature = Object.keys(def.signatureDefs)[0];
+    const outputs = def.signatureDefs[signature].outputs;
     // load model
-    log.info('Loading saved model:', modelPath);
-    model = await tf.node.loadSavedModel(modelPath, tags, signature);
-    model.dtype = Object.values(def.signatureDefs[signature].inputs)[0]['dtype']
-    model.name = modelPath;
-    model.map = map;
-    models[modelPath] = model;
-  } else {
-    model = models[modelPath];
+    try {
+      models[modelPath] = await tf.node.loadSavedModel(modelPath, tags, signature);
+    } catch (err) {
+      log.error('Error loading graph model:', modelPath, err.message);
+      models[modelPath] = modelPath;
+      return;
+    }
+    models[modelPath].dtype = Object.values(def.signatureDefs[signature].inputs)[0]['dtype'];
+    models[modelPath].name = modelPath;
+    // build model map
+    models[modelPath].map = {};
+    for (const i in Object.keys(outputs)) models[modelPath].map[Object.keys(outputs)[i]] = i;
+    if (debug) log.data('Model output map:', models[modelPath].map);
   }
-  const t2 = process.hrtime.bigint();
-
   // get image tensor
-  const imageT = getTensorFromImage(image, model.dtype);
-  if (!imageT) return;
-
+  const imageT = getTensorFromImage(image, models[modelPath].dtype);
   // run prediction
-  const resT = await model.predict(imageT, {score: minScore, iou: iouThreshold, topk: maxResults });
-  const t3 = process.hrtime.bigint();
-
+  const t0 = process.hrtime.bigint();
+  let resT;
+  try {
+    resT = models[modelPath].predict ? await models[modelPath].predict(imageT, { score: minScore, iou: iouThreshold, topk: maxResults }) : null;
+  } catch (err) {
+    log.error('Error executing graph model:', modelPath, err.message);
+  }
+  const t1 = process.hrtime.bigint();
   // parse outputs
-  const parsed = await processPrediction(resT, imageT, model, coco);
-  const t4 = process.hrtime.bigint();
-
+  const parsed = resT ? await processPrediction(resT, imageT, models[modelPath], coco) : [];
+  if (parsed.length > 0) perf(modelPath, t1 - t0);
   // free up memory
   tf.dispose(imageT);
   tf.dispose(resT);
-
+  // save processed image
+  if (parsed.length > 0) await saveProcessedImage(image, `${imgOutput}${path.basename(image)}-${path.basename(modelPath)}.jpg`, parsed);
   // results
-  log.data('Timings:', modelPath, 'load:', time(t2, t0), 'predict:', time(t3, t2), 'process:', time(t4, t3));
-  return parsed;
+  if (parsed.length > 0) log.data('Exec:', image, modelPath, Math.trunc(parseInt(t1 - t0) / 1000 / 1000), 'ms', 'detected', parsed.length, 'objects');
 }
 
 async function processGraphModel(image, modelPath) {
-  const t0 = process.hrtime.bigint();
-  // load model
-  let model;
   if (!models[modelPath]) {
-    log.info('Loading graph model:', modelPath);
-    // static map since graph model looses signature info
-    model = await tf.loadGraphModel(`file://${path.join(modelPath, '/model.json')}`);
-    const map = {};
-    const sig = model.executor._signature;
-    map['detection_classes'] = Object.keys(sig['outputs']).findIndex((a) => a === 'Identity_2:0');
-    map['detection_scores'] = Object.keys(sig['outputs']).findIndex((a) => a === 'Identity_4:0');
-    map['detection_boxes'] = Object.keys(sig['outputs']).findIndex((a) => a === 'Identity_1:0');
-    model.dtype = Object.values(sig['inputs'])[0]['dtype']
-    model.name = modelPath;
-    model.map = map;
+    log.state('Loading graph model:', modelPath);
+    // load model
+    try {
+      models[modelPath] = await tf.loadGraphModel(`file://${path.join(modelPath, '/model.json')}`);
+    } catch (err) {
+      log.error('Error loading graph model:', modelPath, err.message);
+      models[modelPath] = modelPath;
+      return;
+    }
+    // static model map since graph model looses signature info
+    const sig = models[modelPath].executor._signature;
+    models[modelPath].map = {};
+    models[modelPath].map['detection_classes'] = Object.keys(sig['outputs']).findIndex((a) => a === 'Identity_2:0');
+    models[modelPath].map['detection_scores'] = Object.keys(sig['outputs']).findIndex((a) => a === 'Identity_4:0');
+    models[modelPath].map['detection_boxes'] = Object.keys(sig['outputs']).findIndex((a) => a === 'Identity_1:0');
+    models[modelPath].dtype = Object.values(sig['inputs'])[0]['dtype'];
+    models[modelPath].name = modelPath;
     if (debug) log.data('Model signature:', sig);
-    if (debug) log.data('Model output map:', map);
-    models[modelPath] = model;
-  } else {
-    model = models[modelPath];
+    if (debug) log.data('Model output map:', models[modelPath].map);
   }
-  const t2 = process.hrtime.bigint();
-
   // get image tensor
-  const imageT = getTensorFromImage(image, model.dtype);
-  if (!imageT) return;
-
+  const imageT = getTensorFromImage(image, models[modelPath].dtype);
   // run prediction
-  const resT = await model.executeAsync(imageT);
-  const t3 = process.hrtime.bigint();
-
+  const t0 = process.hrtime.bigint();
+  let resT;
+  try {
+    resT = models[modelPath].executeAsync ? await models[modelPath].executeAsync(imageT) : null;
+  } catch (err) {
+    log.error('Error executing graph model:', modelPath, err.message);
+  }
+  const t1 = process.hrtime.bigint();
   // parse outputs
-  const parsed = await processPrediction(resT, imageT, model, coco);
-  const t4 = process.hrtime.bigint();
-
+  const parsed = resT ? await processPrediction(resT, imageT, models[modelPath], coco) : [];
+  if (parsed.length > 0) perf(modelPath, t1 - t0);
   // free up memory
   tf.dispose(imageT);
   tf.dispose(resT);
-
+  // save processed image
+  if (parsed.length > 0) await saveProcessedImage(image, `${imgOutput}${path.basename(image)}-${path.basename(modelPath)}.jpg`, parsed);
   // results
-  log.data('Timings:', modelPath, 'load:', time(t2, t0), 'predict:', time(t3, t2), 'process:', time(t4, t3));
-  return parsed;
-}
-
-/*
-async function processLayersModel(image, modelPath) {
-  // needs rewrite
-  log.info('Loading layers model:', modelPath);
-  const model = await tf.loadLayersModel(`file://${path.join(__dirname, modelPath)}`);
-  log.data('Model signature:', model.executor._signature);
-  log.state('TensorFlow/JS Memory', tf.memory());
-  const res = await model.executeAsync(image);
-  log.data('Detected', res);
-}
-*/
-
-async function testSavedModel(image, model) {
-  const data = await processSavedModel(image, model);
-  // log.data('Results', model, data.map((a) => [a.score, a.class]));
-  if (debug) log.data('Results:', model, 'detected', data.length, 'objects');
-  await saveProcessedImage(image, `${imgOutput}${path.basename(image)}-${path.basename(model)}.jpg`, data);
-}
-
-async function testGraphModel(image, model) {
-  const data = await processGraphModel(image, model);
-  // log.data('Results', model, data.map((a) => [a.score, a.class]));
-  if (!data) return;
-  log.data('Results:', model, 'detected', data.length, 'objects');
-  await saveProcessedImage(image, `${imgOutput}${path.basename(image)}-${path.basename(model)}.jpg`, data);
+  if (parsed.length > 0) log.data('Exec:', image, modelPath, Math.trunc(parseInt(t1 - t0) / 1000 / 1000), 'ms', 'detected', parsed.length, 'objects');
 }
 
 async function main() {
   log.header();
   log.info('TensorFlow/JS Version', tf.version_core);
-  await tf.setBackend('tensorflow'); // 'tensorflow' is tfjs-node backend mapped to libtensorflow.so
+  // await tf.setBackend('tensorflow'); // 'tensorflow' is tfjs-node backend mapped to libtensorflow.so
   await tf.enableProdMode();
-  tf.ENV.set('DEBUG', false);
-  tf.ENV.set('WEBGL_FORCE_F16_TEXTURES', true);
+  await tf.ENV.set('DEBUG', false);
+  // tf.ENV.set('WEBGL_FORCE_F16_TEXTURES', true);
   await tf.ready();
   log.info('TensorFlow/JS Backend', tf.getBackend());
   log.info('TensorFlow/JS Flags', tf.ENV.flags);
+  // enumerate all images in input and run prediction loop for each model
+  const images = fs.readdirSync(imgInput);
+  log.info('Images: ', images.length);
+  // enumerate all local models
+  const modelsSaved = fs.readdirSync('models/saved/');
+  const modelsGraph = fs.readdirSync('models/graph/');
+  log.info('Models: Saved: ', modelsSaved.length, 'Graph:', modelsGraph.length);
 
-  const files = fs.readdirSync(imgInput);
-  log.info('Analyzing', files.length, 'images');
-  for (const file of files) {
-    const image = `inputs/${file}`
-    await testSavedModel(image, 'models/ssd-mobilenet-v2-saved');
-    await testGraphModel(image, 'models/ssd-mobilenet-v2-graph');
-    await testGraphModel(image, 'models/ssd-mobilenet-v2-graph-f16');
-    await testGraphModel(image, 'models/ssd-mobilenet-v2-graph-uint16');
-    await testGraphModel(image, 'models/ssd-mobilenet-v2-graph-uint8');
-
-    await testSavedModel(image, 'models/centernet-resnet-50-v2-saved');
-    // await testGraphModel(image, 'models/centernet-resnet-50-v2-graph'); // TypeError: Cannot read property 'children' of undefined at /home/vlado/dev/tfjs/tfjs-converter/dist/tf-converter.node.js:3465:33
-    
-    await testSavedModel(image, 'models/centernet-resnet-50-v1-fpn-saved');
-    // await testGraphModel(image, 'models/centernet-resnet-50-v1-fpn-graph'); // TypeError: Cannot read property 'children' of undefined at /home/vlado/dev/tfjs/tfjs-converter/dist/tf-converter.node.js:3465:33
-
-    await testSavedModel(image, 'models/centernet-resnet-101-v1-fpn-saved');
-    // await testGraphModel(image, 'models/centernet-resnet-101-v1-fpn-graph'); // TypeError: Cannot read property 'children' of undefined at /home/vlado/dev/tfjs/tfjs-converter/dist/tf-converter.node.js:3465:33
-
-    await testSavedModel(image, 'models/efficientdet-d0-saved');
-    // await testGraphModel(image, 'models/efficientdet-d0-graph'); // TypeError: Cannot read property 'children' of undefined at /home/vlado/dev/tfjs/tfjs-converter/dist/tf-converter.node.js:3465:33
-
-    await testSavedModel(image, 'models/efficientdet-d5-saved');
-    // await testGraphModel(image, 'models/efficientdet-d5-graph'); // TypeError: Cannot read property 'children' of undefined at /home/vlado/dev/tfjs/tfjs-converter/dist/tf-converter.node.js:3465:33
-
-    await testSavedModel(image, 'models/faster-rcnn-resnet-50-v1-saved'); 
-    // await testGraphModel(image, 'models/faster-rcnn-resnet-50-v1-graph'); // ValueError: Unsupported Ops in the model before optimization: BroadcastArgs
-
-    await testSavedModel(image, 'models/retinanet-resnet-50-v1-saved');
-    await testGraphModel(image, 'models/retinanet-resnet-50-v1-graph');
-    await testGraphModel(image, 'models/retinanet-resnet-50-v1-graph-f16');
-    await testGraphModel(image, 'models/retinanet-resnet-50-v1-graph-uint16');
-    await testGraphModel(image, 'models/retinanet-resnet-50-v1-graph-uint8');
-
-    // await testSavedModel(image, 'models/ssd-mobilenet-v2-oi'); // session fail to run with error: Table not initialized.
-    // await testGraphModel(image, 'models/ssd-mobilenet-v2-oi-graph'); // no tags in saved model
-
-    // await testSavedModel(image, 'models/faster-rcnn-inception-resnet-v2-oi'); // session fail to run with error: Table not initialized.
-    // await testGraphModel(image, 'models/faster-rcnn-inception-resnet-v2-oi-graph'); // no tags in saved model
+  // loop through all images and saved models
+  performances = {};
+  for (const image of images) {
+    for (const model of modelsSaved) await processSavedModel(`inputs/${image}`, `models/saved/${model}`);
   }
+  // print performance info and dispose models
+  for (const [model, data] of Object.entries(performances)) log.info(model, data);
   for (const model in models) tf.dispose(model);
-  log.info('Done');
+
+  // loop through all images and graph models
+  performances = {};
+  for (const image of images) {
+    for (const model of modelsGraph) await processGraphModel(`inputs/${image}`, `models/graph/${model}`);
+  }
+  // print performance info and dispose models
+  for (const [model, data] of Object.entries(performances)) log.info(model, data);
+  for (const model in models) tf.dispose(model);
 }
 
 main();
